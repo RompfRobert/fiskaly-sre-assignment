2b6cf7c2ae245a14654e43d07a89e04393099883
Robert Rompf <robert.rompf.1998@gmail.com>
2026-03-12 23:41:12 +0100

    Fix formatting after failed lint check

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "fiskaly-sre-assignment-terraform-backend"
-    key     = "terraform.tfstate"
-    region  = "eu-central-1"
-    encrypt = true
+    bucket       = "fiskaly-sre-assignment-terraform-backend"
+    key          = "terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
